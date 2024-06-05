793eca43fba058b2cacaa45e3984dab4eed3b506
a-hr <heerreroalvaro@gmail.com>
2024-06-05 11:44:02 +0200

    added input params file

--- launch.sh
+++ launch.sh
@@ -9,4 +9,4 @@ module load Singularity Nextflow Go
 export NXF_OPTS="-Xms500M -Xmx8G"
 
 # launch the main process
-nextflow run main.nf -resume -profile cluster 
+nextflow run main.nf -resume -profile cluster -params-file input_params.yaml
